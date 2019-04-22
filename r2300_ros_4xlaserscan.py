55511eb5b635ebf156f62cf2aea8d66d31b53caf
l20031018 <13911713119@139.com>
2019-04-22 13:11:51 +0800

    Add files via upload

--- r2300_ros_4xlaserscan.py
+++ r2300_ros_4xlaserscan.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python
 
+'''
+Pepperl-Fuchs  R2300  ROS driver
+Frame id:R2300_datas
+Topic:R2300/data(msg type:LaserScan, output:layer2)
+test on ros_indigo python 2.7
+
+author: Ding Yi
+email:l20031018@126.com
+
+'''
+
 import rospy
 from sensor_msgs.msg import LaserScan
 import struct as st
@@ -13,16 +24,17 @@ import random
 import bisect
 
 mute = Lock()
-port=random.randint(16000,19000)
+port = random.randint(16000, 19000)
 q = Queue.Queue(200)
 q1 = Queue.Queue(200)
-pc=LaserScan()
+pc = LaserScan()
 print pc.header
 conf = ConfigParser.ConfigParser()
 conf.read('conf.ini')
 ip = conf.get('ip_address', 'ip')
 local_ip = conf.get('ip_address', 'local_ip')
-request_handle = 'http://' + ip+ '/cmd/request_handle_udp?address=' + local_ip + '&port=' + str(port)+' HTTP/1.0'
+selected_layer = int(conf.get('option', 'layer'))
+request_handle = 'http://' + ip + '/cmd/request_handle_udp?address=' + local_ip + '&port=' + str(port) + ' HTTP/1.0'
 request_scanoutput = 'http://' + ip + '/cmd/start_scanoutput?handle=default HTTP/1.0'
 set_f_l = 'http://' + ip + '/cmd/set_parameter?scan_frequency=50&pilot_enable=off'
 
@@ -33,72 +45,59 @@ def getvalue(points):
     intensities = []
     i = 0
     #print len(points[0:4])
-    while i<len(points):
+    while i < len(points):
 
-        s=st.unpack('<I',points[i:i+4])
+        s = st.unpack('<I',points[i:i+4])
         intensities.append((s[0]>>20)&0xfff)
         distance.append(float(s[0]&0xfffff)/1000)
-        i+=4
-    return distance,intensities
+        i += 4
+    return distance, intensities
 
 def pailie():
-    global mute,q
-    layer0=[]
-    layer1=[]
-    layer2=[]
-    layer3=[]
-    iS_layer_full=0
-    point=b''
-    while iS_layer_full!=1:
-        #mute.acquire()
-        if q.qsize!=0:
-            try:
-                e=q.get(timeout=1)
-            except:
-                print 'q get error'
-            #mute.release()
+    global mute, q, selected_layer
+    layer0 = []
+    layer1 = []
+    layer2 = []
+    layer3 = []
+    iS_layer_full = 0
+    while iS_layer_full != 1:
 
-        else:
-            mute.release()
-            pass
-        #print 'points in this pack is %d' %len(e[-1])
-        if e[2]==0:
-            bisect.insort_right(layer0,e)
-        if e[2]==1:
-            bisect.insort_right(layer1,e)
-        if e[2]==2:
-            bisect.insort_right(layer2,e)
-        if e[2]==3:
-            bisect.insort_right(layer3,e)
-        if len(layer0)==4 and len(layer1)==4 and len(layer2)==4 and len(layer3)==4:
-            iS_layer_full=1
+        try:
+            e = q.get()
+        except:
+            print 'q get error'
+        if e[2] == selected_layer and len(layer2) < 4:
+            bisect.insort_right(layer2, e)
+        if len(layer2) == 4:
+            iS_layer_full = 1
 
+    return layer2[0][-1] + layer2[1][-1] + layer2[2][-1] + layer2[3][-1]
 
-    return layer0[0][-1]+layer0[1][-1]+layer0[2][-1]+layer0[3][-1],layer1[0][-1]+layer1[1][-1]+layer1[2][-1]+layer1[3][-1],layer2[0][-1]+layer2[1][-1]+layer2[2][-1]+layer2[3][-1],layer3[0][-1]+layer3[1][-1]+layer3[2][-1]+layer3[3][-1]
 
 def from_bytes(data):
-    package_header=st.unpack('<HHIHHHHQIIHHHii',data[0:46])
-    num_points_packet=package_header[11]
-    layer_index=package_header[6]
-    header_size=package_header[3]    #not sure
-    angular_increment=package_header[-1]
-    packet_number=package_header[5]
-    scan_number=package_header[4]
-    point=data[header_size:]
-    first_index=package_header[12]
-    first_angle=package_header[13]
-    return [packet_number,scan_number,layer_index,num_points_packet,first_index,first_angle,angular_increment,point]
+    package_header = st.unpack('<HHIHHHHQIIHHHii',data[0:46])
+    num_points_packet = package_header[11]
+    layer_index = package_header[6]
+    header_size = package_header[3]    #not sure
+    angular_increment = package_header[-1]
+    packet_number = package_header[5]
+    scan_number = package_header[4]
+    point = data[header_size:]
+    first_index = package_header[12]
+    first_angle = package_header[13]
+    return [packet_number, scan_number, layer_index, num_points_packet, first_index, first_angle, angular_increment, point]
+
 
 def request_datas():
     response = urllib2.urlopen(set_f_l)
     response = urllib2.urlopen(request_handle)
     j = response.read()
-    result=json.loads(j)
-    if result["error_code"]==0 and result["error_text"]=='success':
+    result = json.loads(j)
+    if result["error_code"] == 0 and result["error_text"] == 'success':
         response = urllib2.urlopen(request_scanoutput)
         j = response.read()
-        result=json.loads(j)
-        if result["error_code"]==0 and result["error_text"]=='success':
+        result = json.loads(j)
+        if result["error_code"] == 0 and result["error_text"] == 'success':
             print 'start scanoutput done'
         else:
             print 'fail'
@@ -115,49 +114,30 @@ class ProducerThread(Thread):
         global local_ip, port, q
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.bind(("10.0.10.6", port))
-        print 'Bind UDP On Port 16435..'
+        print 'Bind UDP On Port %d..' %port
         while True:
             data, addr = s.recvfrom(2000)
-            #print 'lenth of data is %d' %len(data)
-            #if data[0:2] == b'\x5c\x2a':
-                #data += s.recvfrom()(st.unpack('<I', data[4:]))
             a = from_bytes(data)
-            #print a
-            #print len(a[-1])
             q.put(a)
 
 
 class ConsumerThread(Thread):
 
-    #def __init__(self):
-    #    super(ConsumerThread, self).__init__()
-
-
     def run(self):
-        global q,q1
+        global q, q1
         while True:
-            #mute.acquire()
-            if q.qsize!=0:
-                s=pailie()[2]
-                print '3'
-                s1=getvalue(s)
-                print '4'
-                q1.put(s1,timeout=3)
-                print 'put into Q done'
-                print 'Q zise is %d' %q1.qsize()
-                print '5'
-                #mute.release()
-            else:
-                print 'waiting for q'
-                #mute.release()
+            s = pailie()
+            s1 = getvalue(s)
+            try:
+                q1.put(s1, False)
+            except:
                 pass
-
 
 if __name__ == '__main__':
     rospy.init_node("R2300_4")
     pub_data = rospy.Publisher('R2300/data', LaserScan, queue_size=1)
     frame_id = rospy.get_param('~frame_id', 'R2300_datas')
-    frequency = rospy.get_param('frequency', 100)
+    frequency = rospy.get_param('frequency', 20)
     rate = rospy.Rate(frequency)
     seq = 0
     pc.header.frame_id = frame_id
@@ -168,8 +148,8 @@ if __name__ == '__main__':
     pc.range_min = 0.2
     pc.range_max = 10
     request_datas()
-    producer=ProducerThread()
-    consumer=ConsumerThread()
+    producer = ProducerThread()
+    consumer = ConsumerThread()
     producer.start()
     #ProducerThread().join()
     consumer.start()
@@ -177,23 +157,16 @@ if __name__ == '__main__':
 
     print rospy.is_shutdown()
     while 1:
-        print 'q size is %d' %q.qsize()
-        print 'Q size is %d' %q1.qsize()
-        print 'ConsumerThread is ', consumer.is_alive()
-        print 'ProducerThread is ', producer.is_alive()
-        print q1.qsize()!=0
         try:
-            scans=q1.get(timeout=0.01)
-            print 'get from Q done'
-            print '1'
+            scans = q1.get(False)
             pc.header.stamp = rospy.Time.now()
             pc.header.seq = seq
-            pc.ranges=scans[0]
-            pc.intensities=scans[1]
+            pc.ranges = scans[0]
+            pc.intensities = scans[1]
             pub_data.publish(pc)
-            print '2'
-            seq+=1
+            seq += 1
             rate.sleep()
         except:
-            print 'get from Q error'
+            #print 'get from Q error'
+            pass
 
